5edd974813e113efd789f9c30b63a803a65b716a
Adam-Karcz <adam.karcz.1984@gmail.com>
2023-08-26 07:29:20 +0200

    exercises for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Task1 } from './components/Task1'
+import { Task2 } from './components/Task2'
+import { Task3 } from './components/Task3'
+import { Task4 } from './components/Task4'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container--app'><Task1 taskName={'Task1'} taskID={1} doTask={(jakiśParametr) => jakiśParametr}/><br/><br/><Task2 /><br/><br/><Task3 /><br/><br/><Task4 />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
